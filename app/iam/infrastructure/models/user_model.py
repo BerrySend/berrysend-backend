ac687ba1811a37a13626a0958b703de86a4840bd
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-10-30 00:51:53 -0500

    feat(auth): add user model for mapping

---
+++ app/iam/infrastructure/models/user_model.py
@@ -0,0 +1,17 @@
+﻿from sqlalchemy import Column, String
+from sqlalchemy.orm import Mapped
+
+from app.shared.infrastructure.models.base_model import BaseModelORM
+
+class UserModel(BaseModelORM):
+    """
+    Model class for mapping the user entity fields
+
+    Attributes:
+        email (str): the unique email address of the user
+        hashed_password (str): the hashed password of the user
+    """
+    __tablename__ = "users"
+
+    email: Mapped[str] = Column(String(255), unique=True, nullable=False)
+    hashed_password: Mapped[str] = Column(String(255), nullable=False)
