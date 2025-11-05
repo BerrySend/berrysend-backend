99bdc6066f620f7ffb875d85f7fce1fa835fab01
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-11-05 18:47:41 -0500

    feat(ports): add default csv file online reader

---
+++ app/shared/infrastructure/readers/csv_reader.py
@@ -0,0 +1,24 @@
+﻿"""
+Reads CSV files.
+Used when trying to read CSV files and map its contents into the database.
+"""
+from dataclasses import dataclass
+import requests
+import csv
+from io import StringIO
+
+@dataclass
+class CsvFileReader:
+
+    @staticmethod
+    def read(url: str) -> list[dict]:
+        """
+        Opens a CSV file and returns its contents as a list of dictionaries.
+
+        :param url: The url to access the CSV file.
+        :return: The list of dictionaries with the contents of the CSV file.
+        """
+        response = requests.get(url)
+        response.raise_for_status()
+        f = StringIO(response.text)
+        return list(csv.DictReader(f))
