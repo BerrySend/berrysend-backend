feca57090da6626336db2b294e32d6d379d486aa
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-11-06 00:59:17 -0500

    chore: add alembic for database migrations

--- app/config.py
+++ app/config.py
@@ -10,16 +10,16 @@ class Settings(BaseSettings):
     EXPORTS_CSV_URL: str = "https://miservidor.com/data/ports.csv"
 
     # Database-related settings
-    MYSQL_USER: str = "root"
-    MYSQL_PASSWORD: str = "12345678"
-    MYSQL_HOST: str = "localhost"
-    MYSQL_PORT: str = "3306"
-    MYSQL_DB: str = "berry_send_db"
+    MYSQL_USER: str = "user"
+    MYSQL_PASSWORD: str = "password"
+    MYSQL_HOST: str = "host"
+    MYSQL_PORT: str = "port"
+    MYSQL_DB: str = "db"
 
     # JWT Settings
-    JWT_SECRET_KEY: str = "2_GWZryFB3PP2fGDUSGt7C67imqrhXxLj5oEPKDKqLduGswF365VQNGZBPeUokIFIjJp8Ztee82BiOaT_PXuyg"
-    JWT_ALGORITHM: str = "HS256"
-    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    JWT_SECRET_KEY: str = "secret_key_huh"
+    JWT_ALGORITHM: str = "algorithm"
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 0
 
     def database_url(self) -> str:
         """
@@ -35,7 +35,7 @@ class Settings(BaseSettings):
         """
         Configuration for the Settings class.
         """
-        env_file = "development.env"
+        env_file = "suspicious_file"
         env_file_encoding = "utf-8"
 
 # Global instance for the Settings class
