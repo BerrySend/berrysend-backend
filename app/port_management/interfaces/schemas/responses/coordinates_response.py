32801f672b005baae19538f55ad7a5c6d3f3032a
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-11-20 15:46:29 -0500

    feat(port): add coordinates model and integrate into PortResponse for geographical data handling

---
+++ app/port_management/interfaces/schemas/responses/coordinates_response.py
@@ -0,0 +1,18 @@
+﻿from pydantic import BaseModel, Field
+
+
+class Coordinates(BaseModel):
+    """
+    Manages geographical coordinates.
+
+    This class is used to represent and store geographical coordinates, including
+    latitude and longitude. It ensures structured storage and retrieval of
+    coordinate values, commonly used in geographical data handling.
+
+    :ivar latitude: The latitude coordinate of the port.
+    :type latitude: float
+    :ivar longitude: The longitude coordinate of the port.
+    :type longitude: float
+    """
+    latitude: float = Field(title="The latitude coordinate of the port")
+    longitude: float = Field(title="The longitude coordinate of the port")
--- app/port_management/interfaces/schemas/responses/port_response.py
+++ app/port_management/interfaces/schemas/responses/port_response.py
@@ -1,5 +1,7 @@
 ﻿from pydantic import BaseModel, Field
 
+from app.port_management.interfaces.schemas.responses.coordinates_response import Coordinates
+
 
 class PortResponse(BaseModel):
     """
@@ -9,13 +11,15 @@ class PortResponse(BaseModel):
         id (str): The unique identifier of the port.
         name (str): The name of the port.
         country (str): The country where the port is located.
-        latitude (float): The latitude coordinate of the port.
-        longitude (float): The longitude coordinate of the port.
         type (str): The type of the port (e.g., initial, intermediate, destination).
+        capacity (float): The capacity of the port, in tons.
+        connections (int): The number of connections to other ports.
+        coordinates (Coordinates): The coordinates of the port.
     """
     id: str = Field(title="The unique identifier of the port")
     name: str = Field(title="The name of the port")
     country: str = Field(title="The country where the port is located")
-    latitude: float = Field(title="The latitude coordinate of the port")
-    longitude: float = Field(title="The longitude coordinate of the port")
     type: str = Field(title="The type of the port (e.g., initial, intermediate, destination)")
+    capacity: float = Field(title="The capacity of the port, in tons")
+    connections: int = Field(title="The number of connections to other ports")
+    coordinates: Coordinates = Field(title="The coordinates of the port")
