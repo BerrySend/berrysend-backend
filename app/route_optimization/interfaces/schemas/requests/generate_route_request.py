7e3a6fd8eda8cf4bcd5986ad5c1543b2a23e2467
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-11-20 18:23:16 -0500

    feat(route): add request models for route generation and parameters configuration

---
+++ app/route_optimization/interfaces/schemas/requests/generate_route_request.py
@@ -0,0 +1,25 @@
+﻿from pydantic import BaseModel, Field
+
+
+class GenerateRouteRequest(BaseModel):
+    """
+    Represents a request to generate a route with specific parameters.
+
+    This class is used to encapsulate the necessary information required to
+    generate a route from a specified source to a destination using a certain
+    mode of transportation and a specified routing algorithm.
+
+    :ivar source: The starting point/location for the route.
+    :type source: str
+    :ivar destination: The ending point/location for the route.
+    :type destination: str
+    :ivar mode: The transportation mode to use for the route (e.g., maritime or air)
+    :type mode: str
+    :ivar algorithm_name: The name of the routing algorithm to plan the route
+        (e.g., Dijkstra, A*).
+    :type algorithm_name: str
+    """
+    source: str = Field(title="The starting port for the route.")
+    destination: str = Field(title="The ending port for the route.")
+    mode: str = Field(title="The transportation mode for the route (e.g., maritime or air).")
+    algorithm_name: str = Field(title="The name of the routing algorithm to plan the route (only can be A* or Dijkstra).")
---
+++ app/route_optimization/interfaces/schemas/requests/generate_route_with_bellman_ford_request.py
@@ -0,0 +1,30 @@
+﻿from pydantic import BaseModel, Field
+
+from app.route_optimization.interfaces.schemas.requests.parameters_request import ParametersRequest
+
+
+class GenerateRouteWithBellmanFordRequest(BaseModel):
+    """
+    Represents a request to generate a route using the Bellman-Ford algorithm.
+
+    This class is used to encapsulate all the necessary details needed when requesting
+    a route generation between two ports using a specific mode of transportation and
+    routing algorithm. The request includes the source, destination, mode of transportation,
+    algorithm name, and specific parameters.
+
+    :ivar source: The starting port for the route.
+    :type source: str
+    :ivar destination: The destination port for the route.
+    :type destination: str
+    :ivar mode: The mode of transportation for the route (e.g., maritime, air).
+    :type mode: str
+    :ivar algorithm_name: The name of the routing algorithm to plan the route.
+    :type algorithm_name: str
+    :ivar parameters: The parameters for the route generation.
+    :type parameters: ParametersRequest
+    """
+    source: str = Field(title="The starting port for the route.")
+    destination: str = Field(title="The destination port for the route.")
+    mode: str = Field(title="The mode of transportation for the route (e.g., maritime, air).")
+    algorithm_name: str = Field(title="The name of the routing algorithm to plan the route.")
+    parameters: ParametersRequest = Field(title="The parameters for the route generation.")
