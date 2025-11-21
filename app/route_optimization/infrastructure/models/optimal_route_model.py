c21a9b2d96d9e5b383ab26929870c70eb4a94b01
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-11-20 23:43:16 -0500

    feat(export): create Export model and establish relationships with ports

--- app/route_optimization/infrastructure/models/optimal_route_model.py
+++ app/route_optimization/infrastructure/models/optimal_route_model.py
@@ -1,4 +1,4 @@
-﻿from sqlalchemy import Column, String, Float, JSON
+﻿from sqlalchemy import Column, String, Float, JSON, ForeignKey
 from sqlalchemy.orm import Mapped, relationship
 
 from app.shared.infrastructure.models.base_model import BaseModelORM
@@ -33,9 +33,9 @@ class OptimalRouteModel(BaseModelORM):
     """
     __tablename__ = "optimal_routes"
 
-    origin_port_id: Mapped[str] = Column(String(255), nullable=False)
+    origin_port_id: Mapped[str] = Column(String(255), ForeignKey("ports.id"))
     origin_port_name: Mapped[str] = Column(String(255), nullable=False)
-    destination_port_id: Mapped[str] = Column(String(255), nullable=False)
+    destination_port_id: Mapped[str] = Column(String(255), ForeignKey("ports.id"))
     destination_port_name: Mapped[str] = Column(String(255), nullable=False)
     route_mode: Mapped[str] = Column(String(255), nullable=False)
     total_distance: Mapped[float] = Column(Float, nullable=False)
